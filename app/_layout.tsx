f5b000db77b7776d778f014200f1e8a8dd1ab4e9
 <chaitrakachari050221@gmail.com>
2026-02-19 11:25:11 +0530

    tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
 import { Stack } from "expo-router";
-import {useFont} from 'expo-fontf'
+import { ActivityIndicator } from "react-native";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+
 export default function RootLayout() {
-  const [fontsLoaded]=useFonts
-  return <Stack />;
+
+  const [fontsLoaded] = useFonts({
+    appFont: require("./../assets/fonts/Outfit-Regular.ttf"),
+    appFontBold: require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <ClerkProvider>
+      <Stack />
+    </ClerkProvider>
+  );
 }
---
+++ services/Colors.tsx
@@ -0,0 +1,5 @@
+export default{
+    PRIMARY:'#1E90FF'
+,
+    WHITE: '#FFFFFF',
+}
